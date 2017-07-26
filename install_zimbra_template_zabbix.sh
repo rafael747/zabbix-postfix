8d02c681f62f79c6f45318ae6fe82e94bb70dd98
Rafael Stefanini Carreira <rafael_carreira11@hotmail.com>
2017-07-26 14:34:41 -0300

    Update install_zimbra_template_zabbix.sh

--- install_zimbra_template_zabbix.sh
+++ install_zimbra_template_zabbix.sh
@@ -2,25 +2,24 @@
 
 set -e
 
-# script que instala o template do zimbra para monitoramento zabbix
-# from: https://github.com/gatespb/zabbix_templates
+# script que instala o template para monitoramento do postfix no zabbix agent
+# Baseado em: https://github.com/gatespb/zabbix_templates
 
-#scripts necessarios
+# scripts necessarios
 
-echo -e "\n#### Instalação do template de zimbra para monitoramento via zabbix ####\n"
+echo -e "\n#### Instalação do template para monitoramento do postfix via zabbix ####\n"
 
 cd /usr/local/sbin
 
 echo -n "Baixando scripts..."
 
-wget -q -N https://raw.githubusercontent.com/rafael747/zabbix-postfix/master/zimbra_postfix.sh >/dev/null
-wget -q -N https://raw.githubusercontent.com/gatespb/zabbix_templates/237d38961a541ba3ab76def95669a500c20ebf55/templates/Zimbra/client/scripts/pygtail.py > /dev/null
+wget -q -N https://raw.githubusercontent.com/rafael747/zabbix-postfix/master/zabbix_postfix.sh >/dev/null
+wget -q -N https://raw.githubusercontent.com/rafael747/zabbix-postfix/master/pygtail.py > /dev/null
 
 echo  "OK!"
 
-
 chmod +x pygtail.py
-chmod +x zimbra_postfix.sh
+chmod +x zabbix_postfix.sh
 
 # regra permitindo zabbix executar comandos
 
@@ -28,11 +27,11 @@ cd /etc/sudoers.d/
 
 echo -n "Baixando regra de sudors..."
 
-wget -q -N https://raw.githubusercontent.com/gatespb/zabbix_templates/237d38961a541ba3ab76def95669a500c20ebf55/templates/Zimbra/client/sudoers.d/zabbix_zimbra >/dev/null
+wget -q -N https://raw.githubusercontent.com/rafael747/zabbix-postfix/master/zabbix_postfix >/dev/null
 
 echo "OK!"
 
-chmod 440 zabbix_zimbra
+chmod 440 zabbix_postfix
 
 # Configuracao do zabbix (mailq infomacao)
 
@@ -40,23 +39,9 @@ cd /etc/zabbix/zabbix_agentd.conf.d/
 
 echo -n "Baixando configuração do zabbix..."
 
-wget -q -N https://raw.githubusercontent.com/gatespb/zabbix_templates/237d38961a541ba3ab76def95669a500c20ebf55/templates/Zimbra/client/zabbix_agentd.conf.d/zimbra_postfix.conf >/dev/null
+wget -q -N https://raw.githubusercontent.com/rafael747/zabbix-postfix/master/zabbix_postfix.conf >/dev/null
 
 echo "OK!"
-# verificar tarefa na cron
-echo -n "Verificando tarefa na cron..."
-
-if cat /var/spool/cron/crontabs/root |grep "Zabbix check" > /dev/null
-then
-	echo "Tarefa já agendada"
-else
-	echo "Não instalada!"
-	echo -n "Instalando tafera na cron..."
-	echo '# Zabbix check' >> /var/spool/cron/crontabs/root
-	echo '*/5 * * * * /usr/local/sbin/zimbra_postfix.sh 1>/dev/null 2>/dev/null' >> /var/spool/cron/crontabs/root
-	echo "OK!"
-fi
-
 
 echo -n "Reiniciando serviços..."
 service sudo restart >/dev/null
@@ -67,3 +52,9 @@ echo "OK!"
 echo "Instalando pflogsumm (visualizador de log do postfix) e bc ..."
 apt-get install pflogsumm bc
 echo "OK!"
+
+
+echo -e "\nInsira a entrada a seguir na cron:\n"
+
+echo '# Zabbix check'
+echo '*/5 * * * * /usr/local/sbin/zabbix_postfix.sh 1>/dev/null 2>/dev/null'
